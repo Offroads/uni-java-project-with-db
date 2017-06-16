c9ecb6d46d3fe1bfd2545540470704285543e2b6
Oyvind999 <roberto.oyvind@gmail.com>
2017-06-16 23:18:10 +0200

    javadoc for filereader

--- src/no/uib/info233/v2017/obe006/oblig4/utils/FileReader.java
+++ src/no/uib/info233/v2017/obe006/oblig4/utils/FileReader.java
@@ -6,16 +6,32 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+/**
+ * FileReader can read the content of txt files.
+ * 
+ * @author obe006
+ *
+ */
 public class FileReader {
 
+	/**
+	 * Constructor for filereader
+	 */
 	public FileReader() {
 	}
 
+	/**
+	 * Reads the file in the given path
+	 * 
+	 * @param path
+	 *            of the txt file
+	 * @return String with the content of the txt file
+	 * @throws IOException
+	 */
 	public String readFile(String path) throws IOException {
 		String string = "";
 		String file = path;
 
-		// Reading
 		InputStream ips = new FileInputStream(file);
 		InputStreamReader ipsr = new InputStreamReader(ips);
 		BufferedReader br = new BufferedReader(ipsr);
